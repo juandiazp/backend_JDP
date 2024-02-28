9468299bfca1dd455648804f942d340bbd86e622
juandiazp <juandiaz25062000@gmail.com>
2024-02-28 15:55:05 -0600

    Primer commit

---
+++ dist/app.js
@@ -0,0 +1,18 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+/*  DotEnv  */
+const dotenv = require('dotenv');
+dotenv.config();
+const port = process.env.PORT;
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+app.listen(port, () => {
+    return console.log(`Express is listening at http://localhost:${3000}`);
+});
+//# sourceMappingURL=app.js.map
